a8804e7dda8c3cd0860cfed59f8f5483dc5f91ca
Maximilian Hoffmann <accounts@maximilianhoffmann.com>
2014-02-14 13:53:35 +0100

    add baseline, font-smoothing, text and background color inputs

--- js/components/lessInputs.js
+++ js/components/lessInputs.js
@@ -2,11 +2,12 @@ var storage = require('./localstorage');
 var state = storage.get('state') || {};
 
 exports.enable = function(inputs) {
-	window.less.modifyVars(state);
 	inputs.forEach(function(input) {
 		if (state[input.id]) input.value = state[input.id];
-		input.addEventListener('input', updateLessVariable, false);
+		if (input.type === 'text') input.addEventListener('input', updateLessVariable, false);
+		if (input.type === 'checkbox') input.addEventListener('change', updateLessVariable, false);
 	});
+	window.less.modifyVars(state);
 };
 
 function updateLessVariable() {
@@ -16,11 +17,18 @@ function updateLessVariable() {
 }
 
 var types = {
-	'text': text
+	'text': text,
+	'checkbox': checkbox
 };
 
 function text(input, state) {
 	state[input.id] = input.value;
+	if (state[input.id] === "") delete state[input.id];
+	return state;
+}
+
+function checkbox(input, state) {
+	state[input.id] = input.checked;
 	if (state[input.id] === "") delete state[input.id];
 	return state;
 }
